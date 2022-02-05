2398f0a65d76039d52bf34b674c44810542b63cd
emirom <meyari73@gmail.com>
2022-02-05 18:13:13 +0330

    user domain added

---
+++ backend/src/user/domain/user.model.ts
@@ -0,0 +1,16 @@
+
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document, Schema as MongooseSchema } from 'mongoose';
+
+
+@Schema()
+export class User {
+  _id: MongooseSchema.Types.ObjectId;
+
+  @Prop()
+  name: string;
+}
+
+export type UserDocument = User & Document;
+
+export const UserSchema = SchemaFactory.createForClass(User);
