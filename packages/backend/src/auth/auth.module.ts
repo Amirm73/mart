b1de1f2e33db775ca2003b527afcbd777c699b36
emirom <meyari73@gmail.com>
2022-06-01 18:50:01 +0430

    fixed auth imports

--- packages/backend/src/auth/auth.module.ts
+++ packages/backend/src/auth/auth.module.ts
@@ -1,12 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './services/auth.service';
 import { AuthResolver } from './resolver/auth.resolver';
-import { UserModule } from 'src/user/user.module';
 import { LocalStrategy } from './local.strategy';
 import { PassportModule } from '@nestjs/passport';
+import { UserModule } from 'src/user/user.module';
 
 @Module({
-  imports: [PassportModule],
-  providers: [ AuthService, AuthResolver, LocalStrategy],
+  imports: [PassportModule, UserModule],
+  providers: [AuthService, AuthResolver, LocalStrategy],
 })
 export class AuthModule {}
