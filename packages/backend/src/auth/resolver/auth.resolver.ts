b97e41020203d880e42d56064dd030a14377c324
emirom <meyari73@gmail.com>
2022-06-11 12:58:05 +0430

    added auth service test

--- packages/backend/src/auth/resolver/auth.resolver.ts
+++ packages/backend/src/auth/resolver/auth.resolver.ts
@@ -1,8 +1,8 @@
-import { UseGuards } from '@nestjs/common';
+// import { UseGuards } from '@nestjs/common';
 import { Args, Mutation, Resolver } from '@nestjs/graphql';
-import { AuthGuard } from '@nestjs/passport';
-import { LoginResponse } from '../dto/login-response';
-import { LoginUserInput } from '../dto/login-User.input';
+// import { AuthGuard } from '@nestjs/passport';
+import { LoginResponse } from '../dto/LoginUser.response';
+import { LoginUserInput } from '../dto/LoginUser.input';
 import { AuthService } from '../services/auth.service';
 
 @Resolver()
@@ -10,7 +10,7 @@ export class AuthResolver {
   constructor(private authService: AuthService) {}
 
   @Mutation(() => LoginResponse)
-  @UseGuards(AuthGuard('local'))
+  // @UseGuards(AuthGuard('local'))
   login(@Args('LoginUserInput') loginUserInput: LoginUserInput) {
     return this.authService.login(loginUserInput);
   }
