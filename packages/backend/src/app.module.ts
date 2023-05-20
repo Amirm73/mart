9c21e28d86fb3794319df188c13d6dc7f97be598
emirom <meyari73@gmail.com>
2023-05-20 16:24:07 +0330

    moved server static module to common module

--- packages/backend/src/app.module.ts
+++ packages/backend/src/app.module.ts
@@ -1,18 +1,13 @@
 import { Module } from '@nestjs/common';
-import { ServeStaticModule } from '@nestjs/serve-static/dist/serve-static.module';
-import { join } from 'path';
 import { AuthModule } from './auth/auth.module';
-import { categoryModule } from './category/category.module';
+import { CategoryModule } from './category/category.module';
 import { CommonModule } from './common/common.module';
+import { HomePageModule } from './homepage/homePage.module';
+import { ImageModule } from './image/image.module';
 import { ProductModule } from './product/product.module';
 import { UserModule } from './user/user.module';
 @Module({
-  imports: [CommonModule, UserModule, AuthModule, categoryModule, categoryModule, ProductModule,
-        ServeStaticModule.forRoot({
-            rootPath: join(__dirname, '..', '/public/'),
-            serveStaticOptions: { index: false },
-        }),
-  ],
+  imports: [CommonModule, UserModule, AuthModule, CategoryModule,ImageModule,ProductModule, HomePageModule ],
   controllers: [],
   providers: [],
 })
